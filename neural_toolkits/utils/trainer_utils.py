520cbb634241d3708b6889094d062cc4684e1749
Duc <anhduc12101992@gmail.com>
2021-11-16 23:30:57 +0900

    prototype for continue training

--- neural_toolkits/utils/trainer_utils.py
+++ neural_toolkits/utils/trainer_utils.py
@@ -14,9 +14,20 @@ from typing import List, Union, Any
 __all__ = ['Trainer', 'Evaluator']
 
 
+class CONSTANTS:
+    MODEL_DICT = 'model_dict'
+    OPTIM_DICT = 'optim_dict'
+    EPOCH = 'epoch'
+    ITERATION = 'iteration'
+    AMP = 'amp'
+    LR_SCHED_DICT = 'lr_sched_dict'
+    CHECKPOINT = 'checkpoint.pt'
+    EMA_CHECKPOINT = 'ema.pt'
+    PKL_METHOD = 'torch'
+
+
 class Trainer(ABC):
     def __init__(self,
-                 model_name: str,
                  nets: Union[T.nn.Module, List[T.nn.Module]],
                  optimizers: Union[T.optim.Optimizer, List[T.optim.Optimizer]],
                  batch_size: int,
@@ -37,10 +48,14 @@ class Trainer(ABC):
                  distributed: bool = False,
                  fp16: bool = False,
                  sample_inputs: List[Any] = None,
+                 model_name: str = None,
                  output_root: str = None,
+                 num_latest_checkpoints: int = -1,
                  backup: Union[str, List[str]] = None,
                  excludes: Union[str, List[str]] = None,
                  includes: Union[str, List[str]] = None,
+                 checkpoint: str = None,
+                 version: int = -1,
                  **kwargs):
         self._nets = nets
         self.optimizers = optimizers
@@ -64,6 +79,9 @@ class Trainer(ABC):
         self.ema = ema
         self.ema_decay = ema_decay
         self.ema_decay_discount = ema_decay_discount
+        self.checkpoint = checkpoint
+        self.version = version
+        self.num_latest_checkpoints = num_latest_checkpoints
 
         if self.distributed:
             self._initialize_distributed_mode()
@@ -99,17 +117,6 @@ class Trainer(ABC):
             else:
                 raise NotImplementedError
 
-        if fp16:
-            try:
-                from apex import amp
-            except ModuleNotFoundError:
-                print('Cannot import apex. To use fp16, NVIDIA apex must be installed')
-                raise
-
-            assert self.device != 'cpu', 'Cannot use fp16 training on CPU!'
-            amp_opt_level = 'O1'
-            self.nets, self.optimizers = amp.initialize(self.nets, self.optimizers, opt_level=amp_opt_level)
-
         if self.ema:
             if isinstance(self.nets, T.nn.Module):
                 self.ema = ntk.utils.ModelEMA(self.nets.parameters(), decay=self.ema_decay,
@@ -127,12 +134,14 @@ class Trainer(ABC):
             if self.distributed:
                 self.sampler = T.utils.data.distributed.DistributedSampler(self.train_set)
 
+        args = inspect.BoundArguments(inspect.signature(DataLoader), kwargs)
         self.train_loader = DataLoader(
             self.train_set,
             batch_size=batch_size,
             sampler=self.sampler,
             num_workers=num_workers,
-            pin_memory=True
+            pin_memory=True,
+            **args.kwargs
         )
         if self.prefetcher:
             if self.device == 'cpu':
@@ -147,7 +156,8 @@ class Trainer(ABC):
                 batch_size=batch_size if val_batch_size is None else val_batch_size,
                 shuffle=False,
                 num_workers=num_workers,
-                pin_memory=True
+                pin_memory=True,
+                **args.kwargs
             )
             if self.prefetcher:
                 if self.device == 'cpu':
@@ -158,9 +168,93 @@ class Trainer(ABC):
         self.mon = mon
         self.logger = logger
         args = inspect.BoundArguments(inspect.signature(self.mon.initialize), kwargs)
-        self.mon.initialize(model_name, output_root, **args.kwargs)
-        if backup is not None:
-            self.mon.backup(backup, ignores=excludes, includes=includes)
+        if self.checkpoint is None:
+            self.mon.initialize(model_name, output_root, **args.kwargs)
+            if backup is not None:
+                self.mon.backup(backup, ignores=excludes, includes=includes)
+        else:
+            self.mon.initialize(current_folder=checkpoint, **args.kwargs)
+
+            if isinstance(self.device, (str, T.device)):
+                map_location = self.device
+            elif isinstance(self.device, int):
+                map_location = T.device('cuda', self.device)
+            else:
+                raise NotImplementedError
+
+            ckpt = mon.load(CONSTANTS.CHECKPOINT, method=CONSTANTS.PKL_METHOD,
+                            version=version, map_location=map_location)
+            self.mon.epoch = ckpt[CONSTANTS.EPOCH]
+            self.mon.iter = mon.epoch * len(train_set)
+            self.mon.num_iters = None
+
+            pretrained = ckpt[CONSTANTS.MODEL_DICT]
+            if isinstance(self._nets, T.nn.Module):
+                if isinstance(pretrained, dict):
+                    self._nets.load_state_dict(pretrained)
+                elif isinstance(pretrained, (list, tuple)):
+                    self._nets.load_state_dict(pretrained[0])
+                else:
+                    raise NotImplementedError
+            elif isinstance(self._nets, (list, tuple)):
+                if isinstance(pretrained, dict):
+                    logger.info(f'There are {len(self._nets)} models but found only one set of parameters')
+                    self._nets[0].load_state_dict(pretrained)
+                elif isinstance(pretrained, (list, tuple)):
+                    if len(pretrained) != len(self._nets):
+                        logger.info(f'Mismatching number of models and sets of parameters. '
+                                    f'There are {len(self._nets)} models but {len(pretrained)} sets.')
+
+                    for model, params in zip(self._nets, pretrained):
+                        model.load_state_dict(params)
+                else:
+                    raise NotImplementedError
+
+            if isinstance(self.optimizers, (list, tuple)):
+                assert len(self.optimizers) == len(ckpt[CONSTANTS.OPTIM_DICT])
+                for opt, state_dict in zip(self.optimizers, ckpt[CONSTANTS.OPTIM_DICT]):
+                    opt.load_state_dict(state_dict)
+            else:
+                self.optimizers.load_state_dict(ckpt[CONSTANTS.OPTIM_DICT])
+
+            if self.lr_scheduler is not None:
+                self.lr_scheduler.load_state_dict(ckpt[CONSTANTS.LR_SCHED_DICT])
+
+            if self.fp16:
+                try:
+                    from apex import amp
+                except ModuleNotFoundError:
+                    print('Cannot import apex. To use fp16, NVIDIA apex must be installed')
+                    raise
+
+                amp.load_state_dict(ckpt[CONSTANTS.AMP])
+
+            if self.ema is not None:
+                ema_sd = mon.load(CONSTANTS.EMA_CHECKPOINT, method=CONSTANTS.PKL_METHOD, version=version)
+                if isinstance(self.ema, (list, tuple)):
+                    assert isinstance(ema_sd, (list, tuple))
+                    if len(ema_sd) != len(self.ema):
+                        logger.warning(f'There are {len(self.ema)} model EMAs but {len(ema_sd)} state dicts!')
+
+                    for ema, ema_sd_ in zip(self.ema, ema_sd):
+                        ema.load_state_dict(ema_sd_)
+                else:
+                    if isinstance(ema_sd, (list, tuple)):
+                        logger.warning(f'There are one model EMA but {len(ema_sd)} state dicts!')
+                        self.ema.load_state_dict(ema_sd[0])
+                    else:
+                        self.ema.load_state_dict(ema_sd)
+
+        if fp16:
+            try:
+                from apex import amp
+            except ModuleNotFoundError:
+                print('Cannot import apex. To use fp16, NVIDIA apex must be installed')
+                raise
+
+            assert self.device != 'cpu', 'Cannot use fp16 training on CPU!'
+            amp_opt_level = 'O1'
+            self.nets, self.optimizers = amp.initialize(self.nets, self.optimizers, opt_level=amp_opt_level)
 
         if sample_inputs is not None:
             sample_inputs = ntk.utils.batch_to_device(sample_inputs, self.device)
@@ -198,21 +292,26 @@ class Trainer(ABC):
 
     def _dump_states(self):
         self.states = {
-            'model_dict': [net.state_dict() for net in self._nets],
-            'optim_dict': [opt.state_dict() for opt in self.optimizers],
-            'epoch': self.mon.epoch,
-            'iteration': self.mon.iter
+            CONSTANTS.MODEL_DICT: [net.state_dict() for net in self._nets],
+            CONSTANTS.OPTIM_DICT: [opt.state_dict() for opt in self.optimizers],
+            CONSTANTS.EPOCH: self.mon.epoch,
+            CONSTANTS.ITERATION: self.mon.iter
         }
         if self.fp16:
-            self.states['amp'] = amp.state_dict()
+            self.states[CONSTANTS.AMP] = amp.state_dict()
 
-        mon.dump('checkpoint.pt', self.states, method='torch', keep=self.kwargs.get('keep', 10))
+        if self.lr_scheduler is not None:
+            self.states[CONSTANTS.LR_SCHED_DICT] = self.lr_scheduler.state_dict()
+
+        mon.dump(CONSTANTS.CHECKPOINT, self.states, method=CONSTANTS.PKL_METHOD, keep=self.num_latest_checkpoints)
         if self.ema is not None:
             if isinstance(self.ema, (list, tuple)):
                 state_dict = [ema.state_dict() for ema in self.ema]
-                mon.dump('ema.pt', state_dict, method='torch', keep=self.kwargs.get('keep', 10))
+                mon.dump(CONSTANTS.EMA_CHECKPOINT, state_dict,
+                         method=CONSTANTS.PKL_METHOD, keep=self.num_latest_checkpoints)
             else:
-                mon.dump('ema.pt', self.ema.state_dict(), method='torch', keep=self.kwargs.get('keep', 10))
+                mon.dump(CONSTANTS.EMA_CHECKPOINT, self.ema.state_dict(),
+                         method=CONSTANTS.PKL_METHOD, keep=self.num_latest_checkpoints)
 
     def train_step(self, **kwargs):
         for batch in mon.iter_batch(self.train_loader):
@@ -262,7 +361,7 @@ class Trainer(ABC):
         self.evaluate(**arguments.kwargs)
 
     def run_training(self, **kwargs):
-        for _ in mon.iter_epoch(range(self.num_epochs)):
+        for _ in mon.iter_epoch(mon.epoch, range(self.num_epochs)):
             self.train_step(**kwargs)
 
         if dist.is_initialized():
@@ -331,17 +430,6 @@ class Evaluator(ABC):
             else:
                 raise NotImplementedError
 
-        if fp16:
-            try:
-                from apex import amp
-            except ModuleNotFoundError:
-                print('Cannot import apex. To use fp16, NVIDIA apex must be installed')
-                raise
-
-            assert self.device != 'cpu', 'Cannot use fp16 training on CPU!'
-            amp_opt_level = 'O1'
-            self.nets = amp.initialize(self.nets, opt_level=amp_opt_level)
-
         self.val_loader = None
         if val_set is not None:
             self.val_loader = DataLoader(
@@ -371,8 +459,9 @@ class Evaluator(ABC):
         else:
             raise NotImplementedError
 
-        pretrained = mon.load('checkpoint.pt', method='torch',
-                              version=version, map_location=map_location)['model_dict']
+        ckpt = mon.load(CONSTANTS.CHECKPOINT, method=CONSTANTS.PKL_METHOD,
+                        version=version, map_location=map_location)
+        pretrained = ckpt[CONSTANTS.MODEL_DICT]
         if isinstance(self._nets, T.nn.Module):
             if isinstance(pretrained, dict):
                 self._nets.load_state_dict(pretrained)
@@ -394,6 +483,26 @@ class Evaluator(ABC):
             else:
                 raise NotImplementedError
 
+        if self.fp16:
+            try:
+                from apex import amp
+            except ModuleNotFoundError:
+                print('Cannot import apex. To use fp16, NVIDIA apex must be installed')
+                raise
+
+            amp.load_state_dict(ckpt[CONSTANTS.AMP])
+
+        if fp16:
+            try:
+                from apex import amp
+            except ModuleNotFoundError:
+                print('Cannot import apex. To use fp16, NVIDIA apex must be installed')
+                raise
+
+            assert self.device != 'cpu', 'Cannot use fp16 training on CPU!'
+            amp_opt_level = 'O1'
+            self.nets = amp.initialize(self.nets, opt_level=amp_opt_level)
+
         if self.ema:
             if isinstance(self._nets, T.nn.Module):
                 self.ema = ntk.utils.ModelEMA(self._nets.parameters(), decay=.999).to(self.device)  # dummy decay
@@ -403,7 +512,7 @@ class Evaluator(ABC):
             else:
                 raise NotImplementedError
 
-            ema_sd = mon.load('ema.pt', method='torch', version=version)
+            ema_sd = mon.load(CONSTANTS.EMA_CHECKPOINT, method=CONSTANTS.PKL_METHOD, version=version)
             if isinstance(self.ema, (list, tuple)):
                 assert isinstance(ema_sd, (list, tuple))
                 for ema, ema_sd_ in zip(self.ema, ema_sd):
