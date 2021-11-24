fb1ffbc71b01b013210cffee1caf359e617497c5
Duc <anhduc12101992@gmail.com>
2021-11-24 18:07:22 +0900

    added revert sync bn

--- neural_toolkits/utils/layer_utils.py
+++ neural_toolkits/utils/layer_utils.py
@@ -1,9 +1,24 @@
 from collections.abc import Iterable
 import torch.nn as nn
+import torch as T
 
 from . import root_logger
 
-__all__ = ['deprecated', 'get_non_none', 'spectral_norm', 'remove_spectral_norm', 'add_custom_repr']
+__all__ = ['deprecated', 'get_non_none', 'spectral_norm', 'remove_spectral_norm', 'add_custom_repr',
+           'revert_sync_batchnorm']
+
+
+class _BatchNormXd(T.nn.modules.batchnorm._BatchNorm):
+    def _check_input_dim(self, input):
+        # The only difference between BatchNorm1d, BatchNorm2d, BatchNorm3d, etc
+        # is this method that is overwritten by the sub-class
+        # This original goal of this method was for tensor sanity checks
+        # If you're ok bypassing those sanity checks (eg. if you trust your inference
+        # to provide the right dimensional inputs), then you can just use this method
+        # for easy conversion from SyncBatchNorm
+        # (unfortunately, SyncBatchNorm does not store the original class - if it did
+        #  we could return the one that was originally created)
+        return
 
 
 def add_custom_repr(cls):
@@ -129,3 +144,35 @@ def remove_spectral_norm(module, name='weight', exclude=None):
             mod = remove_spectral_norm(mod, name, exclude=exclude)
             module.__setattr__(mod_name, mod)
         return module
+
+
+def revert_sync_batchnorm(module):
+    """
+    From https://github.com/pytorch/pytorch/issues/41081#issuecomment-783961547.
+
+    :param module:
+        An instance of `torch.nn.Module`.
+    :return:
+        An instance of `torch.nn.Module` with `SyncBatchNorm` reverted to BatchNorm.
+    """
+    # this is very similar to the function that it is trying to revert:
+    # https://github.com/pytorch/pytorch/blob/c8b3686a3e4ba63dc59e5dcfe5db3430df256833/torch/nn/modules/batchnorm.py#L679
+    module_output = module
+    if isinstance(module, T.nn.modules.batchnorm.SyncBatchNorm):
+        module_output = _BatchNormXd(module.num_features,
+                                     module.eps, module.momentum,
+                                     module.affine,
+                                     module.track_running_stats)
+        if module.affine:
+            with T.no_grad():
+                module_output.weight = module.weight
+                module_output.bias = module.bias
+        module_output.running_mean = module.running_mean
+        module_output.running_var = module.running_var
+        module_output.num_batches_tracked = module.num_batches_tracked
+        if hasattr(module, "qconfig"):
+            module_output.qconfig = module.qconfig
+    for name, child in module.named_children():
+        module_output.add_module(name, revert_sync_batchnorm(child))
+    del module
+    return module_output
